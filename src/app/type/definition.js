c81039683022f23fa65343117fc36a78166aff9a
konstantin <krivlenia@targetprocess.com>
2014-05-30 16:18:28 +0300

    add definitionFactory

---
+++ src/app/type/definition.js
@@ -0,0 +1,14 @@
+basis.require('basis.data');
+var definition = function (data) {
+  var key, definition = {};
+  for (key in data)
+  {
+    if (data.hasOwnProperty(key) && (key === 'cells' || key === 'y' || key === 'x') && data[key].items.length)
+    {
+      definition[key] = data[key];
+    }
+  }
+  return new basis.data.Object({data: definition});
+};
+
+module.exports = definition;
