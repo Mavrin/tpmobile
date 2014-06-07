db7af3999cedfd25e95308f82ddaf3505e3cf8ad
Mavrin <krivlenia@gmail.com>
2014-06-07 16:33:03 +0300

    add search box for boards list

--- src/module/boardList/index.js
+++ src/module/boardList/index.js
@@ -1,15 +1,35 @@
 basis.require('basis.router');
 basis.require('app.type');
-basis.require('app.ui')
+basis.require('basis.ui');
+basis.require('basis.data');
 
 // выбранный Board
 var selectedBoard = new basis.data.Value();
 
 // основное представление
-var view = new app.ui.UpdatableNode({
+var view = new basis.ui.Node({
     autoDelegate: true,
-
-    dataSource: basis.data.Value.factory('update', function(node){
+    action: {
+        search: function (event) {
+            var splitByName = app.type.Board.byFilter(function (item) {
+                    var textMatch = function (text, pattern, emptyMatchAll) {
+                        pattern = pattern.trim().toLowerCase();
+                        if (pattern.length === 0) {
+                            return emptyMatchAll;
+                        }
+                        var terms = pattern.split(/\s+/i);
+                        var preparedText = (text || '').toLowerCase();
+                        return terms.length > 0 && terms.every(function (term) {
+                            return preparedText.indexOf(term) >= 0;
+                        });
+                    };
+                    return textMatch(item.data.name, event.target.value, true);
+                }
+            );
+            this.setDataSource(splitByName)
+        }
+    },
+    dataSource: basis.data.Value.factory('update', function (node) {
         return app.type.Board.byOwner(node.data.LoggedUser && node.data.LoggedUser.Id);
     }),
 
@@ -21,7 +41,7 @@ var view = new app.ui.UpdatableNode({
         binding: {
             id: 'data:key',
             name: 'data:',
-            selected: selectedBoard.compute('update', function(node, value){
+            selected: selectedBoard.compute('update', function (node, value) {
                 return node.data.key == value;
             })
         }
@@ -30,7 +50,7 @@ var view = new app.ui.UpdatableNode({
 
 // делаем список активным только когда меню открыто
 // active = true тригирует загрузку dataSource (срабатывает его syncAction)
-app.state.isMenuExpanded.link(view, function(value){
+app.state.isMenuExpanded.link(view, function (value) {
     this.setActive(value);
 });
 
