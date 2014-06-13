6952044320e0fd81d67b7f7b9ee884ced218349d
konstantin <krivlenia@targetprocess.com>
2014-06-13 15:13:58 +0300

    rework  boards search

--- src/module/boardList/index.js
+++ src/module/boardList/index.js
@@ -2,6 +2,7 @@ basis.require('basis.router');
 basis.require('app.type');
 basis.require('basis.ui');
 basis.require('basis.data');
+basis.require('basis.data.dataset');
 
 // выбранный Board
 var selectedBoard = new basis.data.Value();
@@ -9,29 +10,43 @@ var selectedBoard = new basis.data.Value();
 // основное представление
 var view = new basis.ui.Node({
     autoDelegate: true,
+
+    // создаем отложено
+    dataSource: function (view) {
+        // отфильтрованный набор
+        return new basis.data.dataset.Filter({
+            // источник от view
+            active:true,
+            source: basis.data.Value.from(view, 'update', function (view) {
+                // по его view.data.LoggedUser.Id получаем набор всех Board
+                return app.type.Board.byOwner(view.data.LoggedUser && view.data.LoggedUser.Id);
+            })
+            // по умолчанию правило не устанавливаем, отсеиваться ничего не будет
+        });
+    },
+
     action: {
         search: function (event) {
-            var splitByName = app.type.Board.byFilter(function (item) {
-                    var textMatch = function (text, pattern, emptyMatchAll) {
-                        pattern = pattern.trim().toLowerCase();
-                        if (pattern.length === 0) {
-                            return emptyMatchAll;
-                        }
-                        var terms = pattern.split(/\s+/i);
-                        var preparedText = (text || '').toLowerCase();
-                        return terms.length > 0 && terms.every(function (term) {
-                            return preparedText.indexOf(term) >= 0;
-                        });
-                    };
-                    return textMatch(item.data.name, event.target.value, true);
-                }
-            );
-            this.setDataSource(splitByName)
+            var rule = basis.fn.$true;  // по умолчанию все Board
+            var terms = event.target.value.trim().toLowerCase();
+
+            if (terms) {
+                // если ввод содержит непробельные символы
+                // делаем массив слов
+                terms = terms.split(/\s+/);
+
+                // создаем правило-фильтр (замыкание)
+                rule = function (item) {
+                    var text = (item.data.name || '').toLowerCase();
+                    return terms.every(function (term) {
+                        return text.indexOf(term) !== -1;
+                    });
+                };
+            }
+
+            this.dataSource.setRule(rule);
         }
     },
-    dataSource: basis.data.Value.factory('update', function (node) {
-        return app.type.Board.byOwner(node.data.LoggedUser && node.data.LoggedUser.Id);
-    }),
 
     template: resource('./template/list.tmpl'),
 
@@ -47,6 +62,7 @@ var view = new basis.ui.Node({
         }
     }
 });
+
 
 // делаем список активным только когда меню открыто
 // active = true тригирует загрузку dataSource (срабатывает его syncAction)
