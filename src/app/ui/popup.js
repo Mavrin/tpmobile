2ea5b97d281de9473cff4bfadc429b4609204183
Mavrin <krivlenia@gmail.com>
2014-06-15 16:01:55 +0300

    small refactoring

---
+++ src/app/ui/popup.js
@@ -0,0 +1,19 @@
+basis.require('basis.ui');
+basis.require('basis.ui.popup');
+
+var Popup = basis.ui.popup.Popup.subclass({
+    dir: 'left bottom left top',
+    autorotate: true,
+    visibleState:false,
+    template:resource('./popup/popup.tmpl'),
+    toggle:function(element) {
+        if(this.visibleState) {
+            this.hide();
+            this.visibleState = false;
+        } else {
+            this.show(element);
+            this.visibleState = true;
+        }
+    }
+});
+module.exports = {Popup:Popup};
--- src/module/contextSelector/index.js
+++ src/module/contextSelector/index.js
@@ -1,19 +1,24 @@
 basis.require('basis.ui');
-basis.require('basis.ui.popup');
+basis.require('app.ui.popup');
 
-var Popup = basis.ui.popup.Popup;
+var Popup = app.ui.popup.Popup;
+var popupContent = new basis.ui.Node({
+    active: true,
+    template: resource('./template/filterProjectAndTeam.tmpl')
+});
 var popup = new Popup({
     dir: 'left bottom left top',
     autorotate: true,
-    template:resource('template/filterProjectAndTeam.tmpl')
+    childNodes: [popupContent]
 });
+
 module.exports = new basis.ui.Node({
-  template: resource('template/context-selector.tmpl'),
-  binding: {
-  },
-  action: {
-      showPopup:function(){
-          popup.show(this.element);
-      }
-  }
+    template: resource('./template/context-selector.tmpl'),
+    binding: {
+    },
+    action: {
+        togglePopup: function () {
+            popup.toggle(this.element);
+        }
+    }
 });
