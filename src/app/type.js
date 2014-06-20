2dafe53338073ab1e564b075aa8a7ab3aed3e9c6
konstantin <krivlenia@targetprocess.com>
2014-06-20 16:07:32 +0300

    boardSelector

--- src/app/type.js
+++ src/app/type.js
@@ -4,7 +4,8 @@ module.exports = {
     Board: require('./type/board.js'),
     AxisItem: require('./type/axisItem.js'),
     Cell: require('./type/cell.js'),
-    CellItem: require('./type/cellItem.js')
+    CellItem: require('./type/cellItem.js'),
+    Entity: require('./type/entity.js')
 };
 
 // проверка, что все типы, на которые ссылаются другие типы, объявлены
